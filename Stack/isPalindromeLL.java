c8938aa3bcfe5de3d9950a27a25c5e1ba6e600b6
Sharib Shaikh <shareebshaikh12@gmail.com>
2025-07-13 00:11:01 +0530

    palindeome LL

---
+++ Stack/isPalindromeLL.java
@@ -0,0 +1,76 @@
+package Stack;
+
+import java.util.Stack;
+
+public class isPalindromeLL {
+    public static Node head;
+      public static Node tail;
+
+      static class Node {
+            char data;
+            Node next;
+
+            public Node(char data) {
+                  this.data = data;
+                  this.next = null;
+            }
+      }
+
+      public void addFirst(char data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                  head = tail = newNode;
+                  return;
+            }
+            newNode.next = head;
+            head = newNode;
+      }
+
+      public void print() {
+            Node temp = head;
+            while (temp != null) {
+                  System.out.print(temp.data + " -> ");
+                  temp = temp.next;
+            }
+            System.out.print("null\n");
+      }
+
+      public static boolean isPalindrome(Node head) {
+            Stack<Character> s = new Stack<>();
+
+            Node slow = head;
+            boolean isPalindrome = true;
+
+            while (slow != null) {
+                  s.push(slow.data);
+                  slow = slow.next;
+            }
+
+            while (head != null) {
+
+                  char ch = s.pop();
+                  if (head.data == ch) {
+                        isPalindrome = true;
+                  } else {
+                        isPalindrome = false;
+                        break;
+                  }
+                  head = head.next;
+            }
+
+            return isPalindrome;
+      }
+      public static void main(String[] args) {
+            isPalindromeLL ll = new isPalindromeLL();
+
+            ll.addFirst('A');
+            ll.addFirst('B');
+            ll.addFirst('C');
+            ll.addFirst('B');
+            ll.addFirst('A');
+
+            ll.print();
+            System.out.println(isPalindrome(head));
+
+      }
+}
