4235a99f5cd6f3c889e36d790a8f1eef8c93a301
Sharib Shaikh <shareebshaikh12@gmail.com>
2025-09-02 23:48:22 +0530

    added comments

--- Binary_Trees/HeightOfBTree.java
+++ Binary_Trees/HeightOfBTree.java
@@ -1,27 +1,33 @@
 package Binary_Trees;
 import Binary_Trees.BTree.Node;;
 
-
 public class HeightOfBTree {
-    
+
     public static int height(Node root) {
-        
-        if ( root == null ) return 0 ;
+
+        if (root == null)
+            return 0;
 
         int leftHeight = height(root.left);
         int rightHeight = height(root.right);
-        int treeHeight = Math.max(leftHeight, rightHeight) + 1 ;
+        int treeHeight = Math.max(leftHeight, rightHeight) + 1;
 
         return treeHeight;
     }
+
     public static void main(String[] args) {
-        
+
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
         root.right.right = new Node(6);
+
+        System.out.println("Height of tree: " + height(root));
+
+        // 1. Complile :- javac Binary_Trees/*.java
+        // 2. Run :- java Binary_Trees.HeightOfBTree
 
     }
 }
