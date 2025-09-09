6e0ae1fa7ee349138066b7afac66d1d41d8ce949
Sharib Shaikh <shareebshaikh12@gmail.com>
2025-09-10 01:06:55 +0530

    Last Comman ancestor

--- Binary_Trees/LCA.java
+++ Binary_Trees/LCA.java
@@ -6,47 +6,75 @@ import Binary_Trees.BTree.Node;
 
 public class LCA {
     
-    public static boolean getPath(Node root, int n, ArrayList<Node> path){
-         if (root == null) {
-            return false;
-         }
+    // Approach 1 : - O(n) , O(n) --> app2
+    // public static boolean getPath(Node root, int n, ArrayList<Node> path){
+    //      if (root == null) {
+    //         return false;
+    //      }
 
-         path.add(root);
+    //      path.add(root);
 
-         if (root.data == n) {
-            return true;
-         }
+    //      if (root.data == n) {
+    //         return true;
+    //      }
 
-         boolean foundLeft = getPath(root.left, n, path);
-         boolean foundRight = getPath(root.right, n, path);
+    //      boolean foundLeft = getPath(root.left, n, path);
+    //      boolean foundRight = getPath(root.right, n, path);
 
-         if ( foundLeft || foundRight) {
-            return true;
-         }
+    //      if ( foundLeft || foundRight) {
+    //         return true;
+    //      }
 
-         path.remove(path.size()-1);
-         return false;
-    }
+    //      path.remove(path.size()-1);
+    //      return false;
+    // }
 
-    public static Node lca(Node root, int n1, int n2){
+    // public static Node lca(Node root, int n1, int n2){
         
-        ArrayList<Node> path1 = new ArrayList<>();
-        ArrayList<Node> path2 = new ArrayList<>();
+    //     ArrayList<Node> path1 = new ArrayList<>();
+    //     ArrayList<Node> path2 = new ArrayList<>();
 
-        getPath(root,n1,path1); // Path of n1
-        getPath(root,n2,path2);  // Path of n2
+    //     getPath(root,n1,path1); // Path of n1
+    //     getPath(root,n2,path2);  // Path of n2
 
-        // Last Common ancestor
-        int i = 0 ;
-        for (; i < path1.size() && i< path2.size() ; i++) {
-            if (path1.get(i) != path2.get(i)) {
-                break;
-            }
+    //     // Last Common ancestor
+    //     int i = 0 ;
+    //     for (; i < path1.size() && i< path2.size() ; i++) {
+    //         if (path1.get(i) != path2.get(i)) {
+    //             break;
+    //         }
+    //     }
+
+    //     // Last equal Node --> i-1th
+    //     Node lca = path1.get(i-1);
+    //     return lca;
+    // }
+
+    // Approach 2 : - O(n) , O(1)
+    public static Node lca(Node root, int n1, int n2){
+
+        if (root == null ) {
+            return root; // empty tree
         }
 
-        // Last equal Node --> i-1th
-        Node lca = path1.get(i-1);
-        return lca;
+        if ( root.data == n1 || root.data == n2 ) {
+            return root;
+        }
+
+        Node leftLca = lca(root.left, n1, n2);
+        Node rightLca =lca(root.right, n1, n2);
+
+        //  leftLca (leftsubtree) == val  and  rightLca == null 
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        //  rightLca == val and leftLca == null
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+          return root; //if (leftLca != null && rightLca != null)
     }
     public static void main(String[] args) {
 
