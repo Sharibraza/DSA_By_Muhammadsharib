e8eaa13fd898f44ba5a1556e3cab2d88b603f6b6
Sharib Shaikh <shareebshaikh12@gmail.com>
2025-09-18 22:20:31 +0530

    print size of largest bst

---
+++ Binary_Trees/BST/SizeOfLargestBST.java
@@ -0,0 +1,66 @@
+package Binary_Trees.BST;
+
+import Binary_Trees.BST.Bst.*;
+
+public class SizeOfLargestBST {
+
+    static class Info {
+        boolean isBst;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBst, int size, int min, int max) {
+            this.isBst = isBst;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static int maxSize = 0;
+
+    public static Info largestBst(Node root) {
+
+        if (root == null) {
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Info leftInfo = largestBst(root.left);
+        Info rightInfo = largestBst(root.right);
+        int size = leftInfo.size + rightInfo.size + 1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+        if (root.data <= leftInfo.max || root.data >= rightInfo.min) {
+            return new Info(false, size, min, max);
+        }
+
+        if (leftInfo.isBst && rightInfo.isBst) {
+            maxSize = Math.max(maxSize, size);
+            return new Info(true, size, min, max);
+        }
+
+        return new Info(false, size, min, max);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.right = new Node(60);
+        root.left.left = new Node(5);
+        root.left.right = new Node(20);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
+
+        Info info = largestBst(root);
+
+        System.out.println("Largest BST size : " + maxSize);
+
+        // 1. Compile : javac Bst.java SizeOfLargestBST.java
+        // 2. Run : java SizeOfLargestBST.java
+    }
+}
