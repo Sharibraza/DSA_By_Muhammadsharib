7d4c10d1874dcf7b7265de1d66cb4f475b6d2984
Sharib Shaikh <shareebshaikh12@gmail.com>
2025-12-25 22:54:29 +0530

    Add 1 to an integer using Bit Manipulation.

--- Bit_Manipulation/AddOne.java
+++ Bit_Manipulation/AddOne.java
@@ -1,13 +1,31 @@
+// Add 1 to an integer using Bit Manipulation. (Hint : try using Bitwise NOT Operator)
+// -> 
+// The expression -~x will add 1 to an integer x. We know that to get negative of
+// a number, invert its bits and add 1 to it (Remember negative numbers are stored in 2’s
+// complement form), i.e., -x = ~x + 1; -~x = x + 1 (by replacing x by ~x)
+
+// public class Solution {
+//             public static void main(String[] args) {
+//                 int x = 6;
+//                 System.out.println(x + " + " + 1 + " is " + -~x);
+//                 x = -4;
+//                 System.out.println(x + " + " + 1 + " is " + -~x);
+//                 x = 0;
+//                 System.out.println(x + " + " + 1 + " is " + -~x);
+//             }
+//     }
+
 package Bit_Manipulation;
 
 import java.util.Scanner;
 
 public class AddOne {
-    
-    public static void increment( int x){
-        int res = x | 1 ;
+
+    public static void increment(int x) {
+        int res = x | 1;
         System.out.println("Result = " + res);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,5 +33,6 @@ public class AddOne {
         int x = sc.nextInt();
         increment(x);
         sc.close();
+
     }
 }
