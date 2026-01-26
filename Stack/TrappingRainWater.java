a53d70ca58d07218a7044a1fdb259a6a4fc5e12e
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-01-26 23:30:44 +0530

    used stack

--- Stack/TrappingRainWater.java
+++ Stack/TrappingRainWater.java
@@ -5,22 +5,21 @@ import java.util.Stack;
 public class TrappingRainWater {
 
     public static int maxWater(int[] height) {
+
         Stack<Integer> stack = new Stack<>();
         int n = height.length;
         int ans = 0;
+
         for (int i = 0; i < n; i++) {
-            while ((!stack.isEmpty())
-                    && (height[stack.peek()] < height[i])) {
+            while ((!stack.isEmpty()) && (height[stack.peek()] < height[i])) {
+
                 int pop_height = height[stack.peek()];
                 stack.pop();
                 if (stack.isEmpty()) {
                     break;
                 }
-                int distance = i - stack.peek() - 1;
-                int min_height
-                        = Math.min(height[stack.peek()],
-                                height[i])
-                        - pop_height;
+                int distance = i - stack.peek() - 1;   // j-i-1
+                int min_height = Math.min(height[stack.peek()], height[i]) - pop_height;
                 ans += distance * min_height;
             }
             stack.push(i);
@@ -30,6 +29,7 @@ public class TrappingRainWater {
 
     public static void main(String[] args) {
         int arr[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        //           0  1  2  3  4  5  6  7  8  9  10 11
         System.out.println(maxWater(arr));
     }
 }
