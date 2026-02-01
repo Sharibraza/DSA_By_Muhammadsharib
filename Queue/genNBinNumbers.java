ea06873abe13f80df5029d6394dfdec607499450
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-02-01 17:22:52 +0530

    implemented 2nd approach

--- Queue/genNBinNumbers.java
+++ Queue/genNBinNumbers.java
@@ -3,8 +3,12 @@ package Queue;
 import java.util.*;
 
 public class genNBinNumbers {
-     public static void genPrintBinNumbers(int n){    // TC : O(N) [O(total characters printed)]   SC : O(N)
-            Queue<String> q = new LinkedList<String>();
+     public static void genPrintBinNumbers(int n){  
+      
+           // 1.Generates binary strings in increasing “binary order” using a queue (BFS-style generation)
+           //   TC : O(N) [O(total characters printed)]   SC : O(N)
+           
+            Queue<String> q = new LinkedList<>();
             q.add("1");
 
             System.out.print("N = " + n + " :- ");
@@ -15,6 +19,13 @@ public class genNBinNumbers {
                   q.add(s1 + "0");
                   q.add(s1 + "1");
             }
+
+            // 2. Converts each i to binary   TC :  O(n log n)  SC :  O(log n)-[Integer.toBinaryString(i) creates a String whose length is O(log i)]
+
+            // for (int i = 1; i <= n; i++) {
+            //       System.out.print(Integer.toBinaryString(i) + " ");
+            // }
+            
       }
       public static void main(String[] args) {
            int n = 5; 
