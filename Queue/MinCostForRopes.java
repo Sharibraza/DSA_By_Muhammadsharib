01c27adbd9f52c44255f2f63f610ad92c8bb1828
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-02-02 05:24:03 +0530

    added comments

--- Queue/MinCostForRopes.java
+++ Queue/MinCostForRopes.java
@@ -3,8 +3,8 @@ package Queue;
 import java.util.PriorityQueue;
 
 public class MinCostForRopes {
-     public static void minCost(int n, int len[]) {
-            PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+     public static void minCost(int n, int len[]) {     // TC : o(n)  SC : o(n)
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
 
             for (int i = 0; i < len.length; i++) {
                   pq.add(len[i]);
@@ -12,7 +12,7 @@ public class MinCostForRopes {
             int result = 0 ;
 
             while (pq.size() > 1) {
-                int first = pq.poll();
+                int first = pq.poll();  // retrieves and removes the “front” element, but doesn’t throw an exception if the queue is empty.
                 int second = pq.poll();
                 result += first + second ;
                 pq.add(first + second );
