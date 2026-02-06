ecad2420cc5cbb3b4cac820546505905a84d5ea2
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-02-07 01:11:21 +0530

    added comments

--- Greedy_Algorithm/Job_seq_Problem.java
+++ Greedy_Algorithm/Job_seq_Problem.java
@@ -18,14 +18,14 @@ public class Job_seq_Problem {
     }
     public static void main(String[] args) {
         int jobsInfo[][] = {{4,20},{1,10},{1,40},{1,30}};
-
+                         // [0] = deadline, [1]=profit
         ArrayList<Job> jobs = new ArrayList<>();
 
         for (int i = 0; i < jobsInfo.length; i++) {
             jobs.add(new Job(i, jobsInfo[i][1], jobsInfo[i][0]));
         }
 
-        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit );
+        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit );  // Descending order of profits
 
         ArrayList<Integer> seq = new ArrayList<>();
         int time = 0;
@@ -40,7 +40,7 @@ public class Job_seq_Problem {
 
         System.out.println("Max jobs = " + seq.size());
         for (int i = 0; i < seq.size(); i++) {
-            System.out.println(seq.get(i));
+            System.out.print(seq.get(i) + " ");
         }
     }
 }
