2be7f1e44e7214c29d94f0f304a3051f33970a0d
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-02-27 01:22:50 +0530

    added how to Iterate on HashSets and LinkedHashSet

--- Hashing/HashSet_Implementation.java
+++ Hashing/HashSet_Implementation.java
@@ -1,23 +1,54 @@
 package Hashing;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
-
-public class HashSet_Implementation{
+public class HashSet_Implementation {
 
     public static void main(String[] args) {
-        HashSet<Integer> hs = new HashSet<>();
+        // HashSet<Integer> hs = new HashSet<>();
+        // hs.add(1);
+        // hs.add(2);
+        // hs.add(4);
+        // hs.add(3);
+        // hs.add(1);
+        // hs.add(2);
 
-        hs.add(1);
-        hs.add(2);
-        hs.add(4);
-         hs.add(3);
-        hs.add(1);
-        hs.add(2);
+        // System.out.println(hs.size());  // No duplicates
+        // System.out.println(hs);
+        // Iteration on HashSet 
+        HashSet<String> hs = new HashSet<>();
+        hs.add("India");
+        hs.add("UAE");
+        hs.add("Indo");
+        hs.add("UK");
 
-        System.out.println(hs.size());  // No duplicates
-        System.out.println(hs);
+        // 1. Using Iterator()
+        // Iterator itr = hs.iterator();
+        // while(itr.hasNext()){
+        //     System.out.print(itr.next() + " ");
+        // }  // unordered -> Indo UK UAE India 
 
+        System.out.print("HashSet : ");
 
+        // 2. foreach
+        for (String val : hs) {
+            System.out.print(val + " ");
+        }
+
+        System.out.println("");
+        
+
+        // 2. LinkedHashSet
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("India");
+        lhs.add("UAE");
+        lhs.add("Indo");
+        lhs.add("UK");
+
+        System.out.print("LinkedHashSet : ");
+        for (String val : lhs) {
+            System.out.print(val + " ");
+        }
     }
 }
