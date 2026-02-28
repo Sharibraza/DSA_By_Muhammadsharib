18676bbe12147b282101d64f6c7c084d37b088f8
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-03-01 01:02:49 +0530

    add Paths array

---
+++ Heap/PathWithMinEffort.java
@@ -0,0 +1,16 @@
+package Heap;
+
+public class PathWithMinEffort {
+
+    public static void main(String[] args) {
+        int path[][] = {
+            {31, 100, 65, 12, 18},
+            {10, 13, 47, 157, 6},
+            {100, 113, 174, 11, 33},
+            {88, 124, 41, 20, 140},
+            {99, 32, 111, 41, 20}
+        };
+
+        
+    }
+}
