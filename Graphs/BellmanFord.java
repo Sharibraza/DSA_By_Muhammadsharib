29c7c5e468cbc007aa80b4c1d898433390dab813
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-04-29 00:08:05 +0530

    refactor: update Bellman-Ford graph creation

--- Graphs/BellmanFord.java
+++ Graphs/BellmanFord.java
@@ -17,37 +17,73 @@ public class BellmanFord {
         }
     }
 
-    public static void createGraph(ArrayList<Edge> graph[]) {
+    // public static void createGraph(ArrayList<Edge> graph[]) {
 
-        for (int i = 0; i < graph.length; i++) {   // null -> initializing empty arrList
-            graph[i] = new ArrayList<>();
-        }
+    //     for (int i = 0; i < graph.length; i++) {   // null -> initializing empty arrList
+    //         graph[i] = new ArrayList<>();
+    //     }
 
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
-        graph[1].add(new Edge(1, 2, -4));
-        graph[2].add(new Edge(2, 3, 2));
-        graph[3].add(new Edge(3, 4, 4));
-        graph[4].add(new Edge(4, 1, -1));
+    //     graph[0].add(new Edge(0, 1, 2));
+    //     graph[0].add(new Edge(0, 2, 4));
+    //     graph[1].add(new Edge(1, 2, -4));
+    //     graph[2].add(new Edge(2, 3, 2));
+    //     graph[3].add(new Edge(3, 4, 4));
+    //     graph[4].add(new Edge(4, 1, -1));
 
+    // }
+
+    public static void createGraph2(ArrayList<Edge> graph) {
+        graph.add(new Edge(0, 1, 2));
+        graph.add(new Edge(0, 2, 4));
+        graph.add(new Edge(1, 2, -4));
+        graph.add(new Edge(2, 3, 2));
+        graph.add(new Edge(3, 4, 4));
+        graph.add(new Edge(4, 1, -1));
     }
 
-    public static void bellman(ArrayList<Edge> graph[], int src) {
-        int dist[] = new int[graph.length];
+    // 1
+    // public static void bellman(ArrayList<Edge> graph[], int src) {
+    //     int dist[] = new int[graph.length];
+    //     for (int i = 0; i < dist.length; i++) {
+    //         if (i != src) {
+    //             dist[i] = Integer.MAX_VALUE;
+    //         }
+    //     }
+
+    //     int V = graph.length;
+    //     // algo
+    //     for (int i = 0; i < V - 1; i++) {         // O(V)
+
+    //         for (int j = 0; j < graph.length; j++) {
+
+    //             for (int k = 0; k < graph[j].size(); k++) {
+    //                 Edge e = graph[j].get(k);
+    //                 // u, v, wt
+    //                 int u = e.src;
+    //                 int v = e.dest;
+    //                 int wt = e.wt;
+
+    //                 if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+    //                     dist[v] = dist[u] + wt;
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    // 2
+    public static void bellman(ArrayList<Edge> graph, int src, int V) {
+        int dist[] = new int[V];
         for (int i = 0; i < dist.length; i++) {
             if (i != src) {
                 dist[i] = Integer.MAX_VALUE;
             }
         }
 
-        int V = graph.length;
         // algo
-        for (int i = 0; i < V - 1; i++) {         // O(V)
+        for (int i = 0; i < V - 1; i++) {  
 
-            for (int j = 0; j < graph.length; j++) {
-
-                for (int k = 0; k < graph[j].size(); k++) {
-                    Edge e = graph[j].get(k);
+            for (int j = 0; j < graph.size(); j++) {
+                    Edge e = graph.get(j);
                     // u, v, wt
                     int u = e.src;
                     int v = e.dest;
@@ -56,7 +92,6 @@ public class BellmanFord {
                     if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
                         dist[v] = dist[u] + wt;
                     }
-                }
             }
         }
 
@@ -69,9 +104,12 @@ public class BellmanFord {
 
     public static void main(String[] args) {
         int V = 5;
-        ArrayList<Edge> graph[] = new ArrayList[V];
-        createGraph(graph);
+        // ArrayList<Edge> graph[] = new ArrayList[V];  // arr
+        // createGraph(graph);
 
-        bellman(graph, 0);
+        ArrayList<Edge> graph = new ArrayList<>();   // list
+        createGraph2(graph);
+
+        bellman(graph, 0, V);
     }
 }
