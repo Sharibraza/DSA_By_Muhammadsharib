bab59b2b73915ae6b9d07d8d21ca699c6cd1172c
Sharib Shaikh <shareebshaikh12@gmail.com>
2026-05-13 00:09:40 +0530

    implemented tabulation method

--- DynamicProgramming/DP_Fibonnaci.java
+++ DynamicProgramming/DP_Fibonnaci.java
@@ -2,24 +2,42 @@ package DynamicProgramming;
 
 public class DP_Fibonnaci {
 
-    public static int fibo(int n, int[] f) {
+    // 1. Memoization
+    public static int fibo1(int n, int[] f) { // O(n)
 
         if (n == 0 || n == 1) {
             return n;
         }
 
         if (f[n] != 0) {
-            f[n] = fibo(n - 1, f) + fibo(n - 2, f);
+            f[n] = fibo1(n - 1, f) + fibo1(n - 2, f);
         }
 
-        return f[n] = fibo(n - 1, f) + fibo(n - 2, f);
+        return f[n] = fibo1(n - 1, f) + fibo1(n - 2, f);
+    }
+
+    // 2. Tabulation
+    public static int fibo2(int n) {    // O(n)
+
+        int dp[] = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n]; 
     }
 
     public static void main(String[] args) {
-        int n = 6;
+        int n = 5;
 
         // memoization
-        int f[] = new int[n + 1]; // 0,0,0....
-        System.out.println(fibo(n, f));
+        // int f[] = new int[n + 1]; // 0,0,0....
+
+        // System.out.println(fibo1(n, f));
+        System.out.println(fibo2(n));
     }
 }
